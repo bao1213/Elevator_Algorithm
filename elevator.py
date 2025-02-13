278fe8d1b39e4b0ef0dae3919414c4012ede8464
bao <2573243608@qq.com>
2025-02-13 10:58:39 +0800

    修改了Building、Passenger、Elevator类中参数

--- elevator.py
+++ elevator.py
@@ -5,26 +5,72 @@ from pubsub import pub
 import time
 import matplotlib.pyplot as plt
 
-max_floor = 20
+max_floor = 20 #这个后面应该删掉
 sum_time = 0
 finished_p = 0
 time_count = 0
 
 
-class Passenger:
+class Building:
 
+    def __init__(self, total_floors, blind_floors_matrix, elevator_count, floor_heights):
+        self.total_floors = total_floors  # 总楼层数
+        self.blind_floors_matrix = blind_floors_matrix  # 盲层矩阵，二维列表表示每层楼对每台电梯的盲层状态，i表示楼层，j表示电梯号，值为1表示呢是盲层，0表示不是盲层
+        self.elevator_count = elevator_count  # 电梯的台数
+        self.floor_height = 4  # 楼层高度为4m
+        self.floors = [i + 1 for i in range(total_floors)]  # 楼层集合，假设从1到total_floors
+
+    def get_floor_info(self, floor_index):
+        """获取第i层楼的信息"""
+        if floor_index < 1 or floor_index > self.total_floors:
+            raise ValueError("楼层索引超出范围")
+        blind_layers_for_elevators = self.blind_floors_matrix[floor_index - 1]  # 获取第i层的盲层状态
+        return {
+            'floor_number': floor_index,
+            'floor_height': self.floor_height,
+            'blind_layers_for_elevators': blind_layers_for_elevators
+        }
+
+    def is_blind_floor(self, floor_index, elevator_index):
+        """判断第i层是否为第i号电梯的盲层"""
+        if floor_index < 1 or floor_index > self.total_floors:
+            raise ValueError("楼层索引超出范围")
+        if elevator_index < 1 or elevator_index > self.elevator_count:
+            raise ValueError("电梯索引超出范围")
+
+        return self.blind_floors_matrix[floor_index - 1][elevator_index - 1]
+
+    def set_blind_floor(self, floor_index, elevator_index):
+        """设置盲层"""
+        self.blind_floors_matrix[floor_index-1][elevator_index-1] = 1;
+        return;
+
+    def get_total_floors(self):
+        """返回建筑物的总楼层数"""
+        return self.total_floors
+
+    def get_elevator_count(self):
+        """返回建筑物的电梯数量"""
+        return self.elevator_count
+    
+    def get_floor_height(self):
+        """返回建筑物的楼层高度"""
+        return self.floor_height
+
+
+class Passenger:
     count = 0
     global time_count
 
     def __init__(self, req_floor=0, arr_floor=0, req_time=0):
         Passenger.count += 1
-        self.id = self.count
-        self.req_time = req_time
-        self.ser_time = 0
-        self.arr_time = 0
-        self.req_floor = req_floor
-        self.arr_floor = arr_floor
-        self.direction = self.set_direction()
+        self.id = self.count  # 乘客编号
+        self.req_time = req_time  # 出发时间
+        self.ser_time = 0  # 服务时间
+        self.arr_time = 0  # 到达时间
+        self.req_floor = req_floor  # 出发层数
+        self.arr_floor = arr_floor  # 到达层数
+        self.direction = self.set_direction()  # 方向 -1:向下 1:向上
 
     def get_total_time(self):
         return self.arr_time - self.req_time
@@ -40,19 +86,24 @@ class Passenger:
 
 
 class Elevator:
-    def __init__(self):
-        self.max_floor = max_floor
-        self.req_que = []
-        self.cur_floor = 1.0
-        # 速度为每时间步上升/下降多少层
-        self.speed = 0.2
-        self.wait = 0
-        self.height_per_floor = 3
-        self.door = True
-        self.avail = True
-        self.tar_floor = None
-        self.direction = 0
-        self.carried = []
+    def __init__(self, max_floor, mode='normal'):
+        self.height_per_floor = 4  # 每层楼的高度，单位是米
+
+        self.max_floor = max_floor  # 最大楼层
+        self.speed = 0.5  # 速度为每时间步上升/下降多少层
+        self.door_time = 2  # 开门/关门的时间延迟为2s
+        self.pass_time = 2 # 电梯上/下乘客的时长为2s
+        self.req_que = []  # 电梯请求队列，用于存储乘客的请求
+        self.cur_floor = 1.0  # 当前楼层，初始化为1层
+        self.wait = 0  # 电梯等待计数，表示等待状态的时间  
+        self.door = True  # 电梯门状态，True表示门是开着的
+        self.avail = True  # 电梯是否可用，True表示可用
+        self.tar_floor = None  # 目标楼层，初始化为空
+        self.direction = 0  # 电梯方向，0代表停止，1代表上行，-1代表下行
+        self.carried = []  # 当前载客情况，存储正在电梯内的乘客
+        self.max_weight = 10  # 电梯最大载重为10个人
+        self.current_weight = 0  # 当前电梯内的总人数
+        self.mode = mode  # 电梯工作模式，上行高峰/下行高峰等
 
     def down(self):
         self.cur_floor = round(self.cur_floor - self.speed, 10)
@@ -76,12 +127,13 @@ class Elevator:
     def open_door(self):
         print('开门')
         if self.door:
-            self.door = False
+            self.door = False  # 门关了，表示电梯到达目标楼层
+            self.wait = self.door_time  # 设置等待时间为开门时间
 
     def close_door(self):
         print('关门')
-        self.door = True
-        self.wait = 0
+        self.door = True  # 门关闭
+        self.wait = self.door_time  # 设置等待时间为关门时间
 
     # 检查电梯需求列表，有没有人上，有没有人下
     def check_passengers(self):
@@ -91,6 +143,7 @@ class Elevator:
         print('检查乘客状态')
         print('请求列表：', [p.id for p in self.req_que])
         print('电梯乘客：', [p.id for p in self.carried])
+
         for p in self.carried[:]:
             if p.arr_floor == self.cur_floor:
                 print('{0}号乘客下电梯'.format(p.id))
@@ -234,7 +287,8 @@ def make_request(elevators, clock, pro):
 
         dis_list = [ele.alloc_cost(p.req_floor, p.direction) for ele in elevators]
         print(dis_list)
-        print('控制台信息：{0}楼用户请求使用{1}号电梯到达{2}楼，ID: {3}'.format(req_floor, dis_list.index(min(dis_list)), arr_floor, p.id))
+        print('控制台信息：{0}楼用户请求使用{1}号电梯到达{2}楼，ID: {3}'.format(req_floor, dis_list.index(min(dis_list)),
+                                                                              arr_floor, p.id))
         elevators[dis_list.index(min(dis_list))].req_que.append(p)
         print("-----------------------")
         print("{}号电梯重新搜索请求".format(dis_list.index(min(dis_list))))
@@ -244,14 +298,26 @@ def make_request(elevators, clock, pro):
 
 def draw_elevator(elevators, ax):
     ax.axis('off')
-    ax.add_patch(plt.Rectangle((0, elevators[0].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[0].door, edgecolor='blue'))
-    ax.add_patch(plt.Rectangle((0.1, elevators[0].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[0].door, edgecolor='blue'))
+    ax.add_patch(
+        plt.Rectangle((0, elevators[0].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[0].door,
+                      edgecolor='blue'))
+    ax.add_patch(
+        plt.Rectangle((0.1, elevators[0].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[0].door,
+                      edgecolor='blue'))
 
-    ax.add_patch(plt.Rectangle((0.4, elevators[1].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[1].door, edgecolor='blue'))
-    ax.add_patch(plt.Rectangle((0.5, elevators[1].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[1].door, edgecolor='blue'))
+    ax.add_patch(
+        plt.Rectangle((0.4, elevators[1].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[1].door,
+                      edgecolor='blue'))
+    ax.add_patch(
+        plt.Rectangle((0.5, elevators[1].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[1].door,
+                      edgecolor='blue'))
 
-    ax.add_patch(plt.Rectangle((0.8, elevators[2].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[2].door, edgecolor='blue'))
-    ax.add_patch(plt.Rectangle((0.9, elevators[2].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[2].door, edgecolor='blue'))
+    ax.add_patch(
+        plt.Rectangle((0.8, elevators[2].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[2].door,
+                      edgecolor='blue'))
+    ax.add_patch(
+        plt.Rectangle((0.9, elevators[2].cur_floor / max_floor - 1.0 / max_floor), 0.1, 0.1, fill=elevators[2].door,
+                      edgecolor='blue'))
 
     plt.savefig("elevator.png")
     # plt.show()
@@ -383,7 +449,7 @@ class ElevatorFrame(wx.Frame):
         temp = image.ConvertToBitmap()
         self.bmp.SetBitmap(temp)
         if finished_p > 0:
-            self.time_text.SetLabel("当前平均周转时间: {}".format(round(sum_time/finished_p, 10)))
+            self.time_text.SetLabel("当前平均周转时间: {}".format(round(sum_time / finished_p, 10)))
         for i, elevator in enumerate(elevators):
             status_label = self.split_line + "\n当前楼层\n{}\n".format(round(elevator.cur_floor))
             status_label += self.split_line + "\n当前电梯上乘客\n"
@@ -402,6 +468,7 @@ class ElevatorFrame(wx.Frame):
 
     def onStopButton(self, evt):
         self.thread.stop()
+
 
 if __name__ == '__main__':
     app = wx.App()
